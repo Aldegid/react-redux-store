e43d3debe2fb13270bdabeae6f4cf60b9c0ad3d9
Aldegid <aldegid@gmail.com>
2019-06-09 17:16:25 +0300

    read from redux store

--- src/components/pages/home-page.js
+++ src/components/pages/home-page.js
@@ -3,19 +3,7 @@ import React from 'react';
 import BookLIst from '../book-list';
 
 const HomePage = () => {
-  const books = [
-    {
-      id: 1,
-      title: 'Book 1',
-      author: 'Aurhor 1'
-    },
-    {
-      id: 2,
-      title: 'Book 2',
-      author: 'Aurhor 2'
-    }
-  ];
-  return <BookLIst books={books} />;
+  return <BookLIst />;
 };
 
 export default HomePage;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,21 @@
 const initialState = {
-  books: []
+  books: [
+    {
+      id: 1,
+      title: 'Book 1',
+      author: 'Aurhor 1'
+    },
+    {
+      id: 2,
+      title: 'Book 2',
+      author: 'Aurhor 2'
+    },
+    {
+      id: 3,
+      title: 'Book 3',
+      author: 'Aurhor 3'
+    }
+  ]
 };
 
 const reducer = (state = initialState, action) => {
