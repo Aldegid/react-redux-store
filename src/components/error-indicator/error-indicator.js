b7c16da5fcb4741f9ad15476e505cb2c1e20e06a
Aldegid <aldegid@gmail.com>
2019-06-09 14:53:00 +0300

    create project structure

---
+++ src/components/error-indicator/error-indicator.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ErrorIndicator = () => {
+  return (
+    <div className='error-indicator'>
+      <h2>Error</h2>
+    </div>
+  );
+};
+
+export default ErrorIndicator;
