c01fc0eff964445d54733bd4b37566d358023da9
Aldegid <aldegid@gmail.com>
2019-06-11 15:00:22 +0300

    update store elements in array

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -6,6 +6,30 @@ const initialState = {
   orderTotal: 220
 };
 
+const updateItemsInCart = (cartItems, newItem, index) => {
+  if (index === -1) {
+    return [...cartItems, newItem];
+  }
+  return [...cartItems.slice(0, index), newItem, ...cartItems.slice(index + 1)];
+};
+
+const updateItem = (book, item) => {
+  if (item) {
+    return {
+      ...item,
+      count: item.count + 1,
+      total: item.total + book.price
+    };
+  } else {
+    return {
+      id: book.id,
+      name: book.title,
+      count: 1,
+      total: book.price
+    };
+  }
+};
+
 const reducer = (state = initialState, action) => {
   console.log(action.type);
   switch (action.type) {
@@ -33,16 +57,16 @@ const reducer = (state = initialState, action) => {
     case 'BOOK_ADDED_TO_CART':
       const bookId = action.payload;
       const book = state.books.find(book => book.id === bookId);
-      const newItemInCart = {
-        id: book.id,
-        name: book.title,
-        count: 1,
-        total: book.price
-      };
+      const itemIndex = state.cartItems.findIndex(({ id }) => id === bookId);
+      const currentItem = state.cartItems[itemIndex];
+
+      const newItemInCart = updateItem(book, currentItem);
+
       return {
         ...state,
-        cartItems: [...state.cartItems, newItemInCart]
+        cartItems: updateItemsInCart(state.cartItems, newItemInCart, itemIndex)
       };
+
     default:
       return state;
   }
