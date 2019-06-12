c21fc83ff2344d0dd7c7b345938e9e849c8cb03b
aldegid <aldegid@gmail.com>
2019-06-12 19:17:38 +0300

    Updates

---
+++ precache-manifest.5a45c4739a33a795ce5d5c36e7927b0c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d547799a780c5859fb658e0a6958e33",
+    "url": "/Aldegid/react-redux-store/index.html"
+  },
+  {
+    "revision": "2a53fcec22c03ca10e10",
+    "url": "/Aldegid/react-redux-store/static/css/main.ccf0c409.chunk.css"
+  },
+  {
+    "revision": "9e5721092808ffac69eb",
+    "url": "/Aldegid/react-redux-store/static/js/2.1418b559.chunk.js"
+  },
+  {
+    "revision": "2a53fcec22c03ca10e10",
+    "url": "/Aldegid/react-redux-store/static/js/main.d681417f.chunk.js"
+  },
+  {
+    "revision": "dd454e8cfc11e025bc9f",
+    "url": "/Aldegid/react-redux-store/static/js/runtime~main.83e27452.js"
+  }
+]);
